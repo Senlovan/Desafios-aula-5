faef087293c6ec58dbf687c990342324ad621e90
Senlovan <maiconmattossenlovan@gmail.com>
2023-05-02 21:32:40 -0300

    Subindo Aula 5

---
+++ index.js
@@ -0,0 +1,63 @@
+/*let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+
+let i = 0
+console.log('d. ', array[i])
+
+array[i+1] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)
+
+const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)*/
+
+
+/*const nick = prompt ("Digite um nome de Usuário")
+const email = prompt ("Digite um e-mail")
+
+console.log ("O e-mail", email, "foi cadastrado com sucesso.Seja Bem Vindo(a),", nick)*/
+
+
+
+
+
+/*let array = ["Pizza", "Xis ", "Arroz ", "Polenta Frita", "Batata Frita"]
+let a = 0
+let b = 1
+let c = 2
+let d = 3
+let e = 4
+console.log ("Essas são as minhas comidas favoritas:")
+
+let novaComida = prompt("Qual a sua Comida Favorita?")
+array[1] = novaComida
+console.log ("a.", array[a])
+console.log("b.", array[b]);
+console.log("c.", array[c]);
+console.log("d:", array[d]);
+console.log("e.", array[e]);*/
+
+
+/*let listaDeTarefas = []
+let tarefas = prompt ("digite 1 tarefa que você deve realizar no dia");
+let tarefa2 = prompt("Digite outra tarefa:");
+let tarefa3 = prompt("Digite mais uma tarefa");
+listaDeTarefas.push(tarefas)
+listaDeTarefas.push(tarefa2)
+listaDeTarefas.push(tarefa3)
+listaDeTarefas.splice(Number (prompt("Digite um némero de 0 a 2 para excluir uma tarefa")),1)
+console.log(listaDeTarefas)*/
+
+
+
+
+
